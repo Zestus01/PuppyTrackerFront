f6bc0c8415429e86caecc9ac4c720fdfd54f1523
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-11-28 21:34:18 +0000

    basic Loading

--- puppyfront/src/App.js
+++ puppyfront/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom/client';
+import DogDisplay from './DogDisplay';
 import './App.css';
 
+const URL = 'https://8000-zestus01-puppytrackerba-c2ujgp24ze0.ws-us77.gitpod.io/dog/';
+
 function App() {
+  const [dogData, setDogData] = useState(null);
+
+  React.useEffect(() =>{
+    axios.get(URL).then((response) => setDogData(response.data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DogDisplay data={dogData}/>
     </div>
   );
 }
